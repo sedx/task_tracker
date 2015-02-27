9e9feca5e8cf42b8f32d5c4e1af7d66407973aa9
Ilya Bondarenko <bondarenko.ik@gmail.com>
2015-02-27 22:11:18 +0500

    Добавил Devise для User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :name, presence: {message: "Роботам здесь не место!"}
   validates :name, length: {minimum: 2 }, unless: lambda{self.name.blank?}
 end
